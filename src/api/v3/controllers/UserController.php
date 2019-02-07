607a51a74aac24fc8d62ac1caf751391602168e0
Vitaliy <theyamshikov@yandex.ru>
2019-02-07 16:27:08 +0600

    make api v3

---
+++ src/api/v3/controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace yii2module\account\api\v3\controllers;
+
+use yii2rails\extension\web\helpers\Behavior;
+use yii2lab\rest\domain\rest\ActiveController as Controller;
+
+class UserController extends Controller
+{
+
+	public $service = 'account.login';
+
+	/**
+	 * @inheritdoc
+	 */
+	public function behaviors() {
+		return [
+			//'cors' => Behavior::cors(),
+			//'authenticator' => Behavior::auth(['view']),
+		];
+	}
+	
+	/**
+	 * @inheritdoc
+	 */
+	public function actions() {
+		return [
+			'create' => [
+				'class' => 'yii2lab\rest\domain\rest\CreateAction',
+			],
+			'view' => [
+				'class' => 'yii2lab\rest\domain\rest\ViewActionWithQuery',
+			],
+		];
+	}
+
+}
