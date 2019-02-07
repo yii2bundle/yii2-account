e4157bdd9418ac72dc47b564bb23ed001dd7c8ae
Vitaliy <theyamshikov@yandex.ru>
2019-02-07 12:30:06 +0600

    clean

--- src/domain/v2/interfaces/repositories/LoginInterface.php
+++ src/domain/v2/interfaces/repositories/LoginInterface.php
@@ -3,10 +3,13 @@
 namespace yii2module\account\domain\v2\interfaces\repositories;
 
 use yii\web\NotFoundHttpException;
+use yii2rails\domain\data\Query;
 use yii2rails\domain\interfaces\repositories\CrudInterface;
 use yii2module\account\domain\v2\entities\LoginEntity;
 
 interface LoginInterface extends CrudInterface {
+	
+	//public function oneByPhone($phone, Query $query = null);
 	
 	/**
 	 * @param string $login
@@ -16,13 +19,14 @@ interface LoginInterface extends CrudInterface {
 	public function isExistsByLogin($login);
 	
 	/**
-	 * @param string $login
+	 * @param string     $login
+	 *
+	 * @param Query|null $query
 	 *
 	 * @return LoginEntity
-	 *
 	 * @throws NotFoundHttpException
 	 */
-	public function oneByLogin($login);
+	//public function oneByLogin($login, Query $query = null);
 	
 	/**
 	 * @param string $token
