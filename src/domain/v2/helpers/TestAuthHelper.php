f1e3300c431ea041b1e86b082540285e998f63b7
Vitaliy <theyamshikov@yandex.ru>
2019-03-03 17:16:00 +0600

    fix

--- src/domain/v2/helpers/TestAuthHelper.php
+++ src/domain/v2/helpers/TestAuthHelper.php
@@ -6,6 +6,7 @@ use Yii;
 use yii2rails\domain\helpers\DomainHelper;
 use yii2rails\extension\web\helpers\ClientHelper;
 use yii2module\account\domain\v2\entities\LoginEntity;
+use yubundle\account\domain\v2\forms\LoginForm;
 
 class TestAuthHelper {
 	
@@ -21,7 +22,10 @@ class TestAuthHelper {
 	
 	public static function authByLogin($login, $password = self::ADMIN_PASSWORD) {
 		self::defineAccountDomain();
-		$userEntity = \App::$domain->account->auth->authentication($login, $password);
+        $loginForm = new LoginForm;
+        $loginForm->login = $login;
+        $loginForm->password = $password;
+		$userEntity = \App::$domain->account->auth->authenticationFromApi($loginForm);
 		Yii::$app->user->setIdentity($userEntity);
 	}
 	
